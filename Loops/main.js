7849e4b8f91b626c4cea590c476507284bf9704a
AndersonDesign1 <josanderson25@gmail.com>
2025-08-15 21:51:07 +0100

    Loops

---
+++ Loops/main.js
@@ -0,0 +1,53 @@
+// For Loop Samples
+
+// for (let i = 0; i < 10; i++) {
+//   console.log("This is loop number " , i);
+// }
+
+// for (let i = 10; i > 0; i--){
+//     console.log(i);
+// }
+
+// console.log("Blast Off! 🚀");
+
+// while loop samples
+// let count = 0;
+// while (count < 3) {
+//     console.log("The count is:", count);
+//     count++;
+// }
+
+// console.log("Loop finished!");
+
+
+// let health = 5;
+// while (health > 0){
+//     console.log("Health is at:", health,"Still alive! Keep fighting");
+//     health--;
+// }
+
+// console.log("Game Over. You died");
+
+
+// For of Loop sample
+// const fruits = ["apple", "banana", "cherry"];
+// for (const fruit of fruits) {
+//     console.log("I love to eat:", fruit);
+// }
+
+
+// // The old way
+// const fruits = ["apple", "banana", "cherry"];
+// for (let i = 0; i < fruits.length; i++) {
+//     console.log("I love to eat:", fruits[i]);
+// }
+
+// //Create the array
+// const favouriteMovies = ["Inception", "Interstellar", "The Dark Knight", "The Matrix",
+//     "The Sandman", "The Flash", "Spiderman: Into the Spider-Verse"];
+// //Set up the for...of loop
+// for (const movie of favouriteMovies) {
+//     console.log("One of my favourite movies is:", movie);
+// }
+
+// console.log("I love movies! 🎬 but these are my all-time favourites:");
